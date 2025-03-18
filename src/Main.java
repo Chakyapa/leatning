927439fb78e736f3ddf89baecd74af851bc67e31
Chakyapa <rararulu@gmail.com>
2025-03-18 17:56:32 +0200

    Exemplu de la Doamna Rotaru corectat

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome !");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util .*;
+import java.awt .*;
+class Atentie extends TimerTask {
+    public void run () {
+        Toolkit.getDefaultToolkit().beep();
+        System.out.print(".");
+    }}
+class Alarma extends TimerTask {
+    public String mesaj ;
+    public Alarma(String mesaj) {
+        this.mesaj = mesaj ;
     }
-}
+    public void run() {
+        System.out.println( mesaj );
+    }}
+public class Main {
+    public static void main(String args []) {
+// Setăm o acţiune repetitivă, cu rată fixă
+        final Timer t1 = new Timer ();
+        t1.scheduleAtFixedRate(
+                new Atentie(), 0, 1*1000) ;
+// Clasă anonima pentru o altă acţiune
+        Timer t2 = new Timer ();
+        t2. schedule ( new TimerTask() {
+            public void run() {
+                System.out.println("Au trecut 10 secunde");
+// Oprim primul timer
+                t1. cancel();
+            }}, 10*1000) ;
+// Setăm o acţiune pentru ora 13:15
+        Calendar calendar = Calendar.getInstance();
+        calendar.set( Calendar . HOUR_OF_DAY, 13);
+        calendar.set( Calendar .MINUTE, 15);
+        calendar.set( Calendar .SECOND, 0);
+        Date ora = calendar.getTime();
+        Timer t3 = new Timer();
+        t3.schedule(new Alarma ("Ora mesei!"), ora );
+    }}
